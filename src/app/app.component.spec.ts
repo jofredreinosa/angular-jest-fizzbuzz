40bf70242e98ce933b5eba9c00675a43721f3356
Jofred <reinosaj@gmail.com>
2025-05-26 16:09:24 -0400

    template + estilos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,4 +10,26 @@ describe('AppComponent', () => {
     expect(fixture).toBeTruthy();
   });
 
+  it('Should return number when is not divisible by 3 or 5', () => {
+    expect(fixture.fizzbuzz(1)).toBe('1');
+  });
+
+  it('Should return "Fizz" when number is divisible by 3', () => {
+    expect(fixture.fizzbuzz(3)).toBe('Fizz');
+  });
+
+  it('Should return "Buzz" when number is divisible by 5', () => {
+    expect(fixture.fizzbuzz(5)).toBe('Buzz');
+  });
+
+  it('Should return "FizzBuzz" when number is divisible by 3 and 5', () => {
+    expect(fixture.fizzbuzz(15)).toBe('FizzBuzz');
+  });
+
+  it('Should generate fizzbuzz list in ngOnInit', () => {
+    expect(fixture.fizzBuzzList).toEqual([]);
+    fixture.ngOnInit();
+    expect(fixture.fizzBuzzList.length).toEqual(100);
+  })
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NgClass
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'fizbuzz';
+export class AppComponent implements OnInit {
+  fizzBuzzList: string[] = [];
+
+  ngOnInit() {
+    this.fizzBuzzList = Array.from({ length: 100}, (_,i) => this.fizzbuzz(i+1));
+  }
+
+  fizzbuzz(number: number): string {
+    if (number % 3 === 0 && number % 5 === 0) {
+      return 'FizzBuzz';
+    }
+    if (number % 3 === 0) {
+      return 'Fizz';
+    }
+    if (number % 5 === 0) {
+      return 'Buzz';
+    }
+    return number.toString();
+  }
 }
